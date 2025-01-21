725bcbc6fd16bbeec0e3bb1a9b8a6ba5468f51b7
Djtv1304 <diegosebastian_t@hotmail.com>
2025-01-21 17:01:17 -0500

    Quitar imports innecesarios

--- src/services/expenses.ts
+++ src/services/expenses.ts
@@ -1,6 +1,3 @@
-import { Expense } from "@/models/expense";
-import { Department } from "@/models/department";
-
 export const fetchExpenses = async (startDate: string, endDate: string) => {
   const response = await fetch(
     `http://localhost:3000/api/expenses?startDate=${startDate}&endDate=${endDate}`
